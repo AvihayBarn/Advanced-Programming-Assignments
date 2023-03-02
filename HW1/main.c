2db3132960f83b024e4b1a49cbbb590dab8fa9ba
Avihay Barnholtz <57184849+AvihayBarn@users.noreply.github.com>
2023-03-02 12:13:14 +0200

    hw1

--- HW1/main.c
+++ HW1/main.c
@@ -1,109 +1,90 @@
 #include "codec.h"
 #include <stdio.h>
-#include <string.h>
 #include <pthread.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+#include "thpool.h"
 
-#define BUFFER_SIZE 1024
-#define THREAD_NUM 4 // c
+#define BUFFER 1024
+#define THREADS 4
 
-int key;
-char *flag;
-// char arr[256][BUFFER_SIZE];
+ 
+typedef struct Package{
+	int key;
+	char flag[2];
+	
+} Package;
 
-typedef struct Task{
-	int index;
-	char buffer[BUFFER_SIZE];
-} Task;
 
-Task taskQueue[256];
-int taskCount = 0;
 
-pthread_mutex_t mutexQueue;
-pthread_cond_t condQueue;
 
-void executeTask(Task* task) {
-	if (strcmp(flag, "-e") == 0){
-		encrypt(task->buffer, key);
-		printf("%s", task->buffer);
-		// memcpy(&arr[task->index], task->buffer, sizeof(task->buffer));
-	}
-	else if (strcmp(flag, "-d") == 0){
-		decrypt(task->buffer, key);
-		printf("%s", task->buffer);
-		// memcpy(&arr[task->index], task->buffer, sizeof(task->buffer));
-	}
-}
+void WritingToFile(void *arg){
 
-void submitTask(Task task) {
-    pthread_mutex_lock(&mutexQueue);
-    taskQueue[taskCount] = task;
-    taskCount++;
-    pthread_mutex_unlock(&mutexQueue);
-    pthread_cond_signal(&condQueue);
-}
-
-void* startThread(void* args) {
-    while (taskCount > 0){
-		Task task;
-
-		pthread_mutex_lock(&mutexQueue);
-		while (taskCount == 0)
-			pthread_cond_wait(&condQueue, &mutexQueue);
-
-		task = taskQueue[0];
-		for (int i = 0; i < taskCount - 1; i++)
-			taskQueue[i] = taskQueue[i + 1];
-		taskCount--;
-		pthread_mutex_unlock(&mutexQueue);
-		executeTask(&task);
-	}
-	return 0;
-}
-
-int main(int argc, char* argv[]) {
-    // Check valid input
-	if (argc != 3)
-		exit(0);
-
-	// Get data from input args
-	key = atoi(argv[1]);
-	flag = argv[2];
-
-	if (strcmp(flag, "-e") != 0 && strcmp(flag, "-d") != 0)
-		exit(0);
+	Package* package = (Package*)arg;
 	
 
-	pthread_t threads[THREAD_NUM];
-	pthread_mutex_init(&mutexQueue, NULL);
-	pthread_cond_init(&condQueue, NULL);
 
-	// Read file from the input
-	char buffer[BUFFER_SIZE]; // Buffer to store data
-	int size = read(0, buffer, BUFFER_SIZE);
-	Task newTask;
-	int index = taskCount + 1; 
-	newTask.index = index;
-	memset(&newTask.buffer,0,BUFFER_SIZE);
-	memcpy(&newTask.buffer, buffer, size);
+	if (strcmp(package->flag , "-e") == 0){
+		printf("%s", buffer);
+		encrypt(buffer, package->key);
+		printf("%s", buffer);
+		
+	}
+	else if (strcmp(package->flag , "-d") == 0){
+		printf("%s", buffer);
+		decrypt(buffer, package->key);
+		printf("%s", buffer);
+		
+	}
+}
 
-	submitTask(newTask);
+int main(int argc, char *argv[])
+{
 	
-	// Create threads
-	for (int i = 0; i < THREAD_NUM; i++)
-		if (pthread_create(&threads[i], NULL, &startThread, NULL) != 0)
-			perror("Failed to create the thread");
+	/*char data[] = "my text to encrypt";
+	int key = 12;
 
-	for (int i = 0; i < THREAD_NUM; i++)
-		if (pthread_join(threads[i], NULL) != 0)	
-			perror("Failed to join the thread");
+	encrypt(data,key);
+	printf("encripted data: %s\n",data);
 
-			
-	pthread_cond_destroy(&condQueue);
-	pthread_mutex_destroy(&mutexQueue);
-	// printf("%s", arr[index]);
+	decrypt(data,key);
+	printf("decripted data: %s\n",data);*/
+	
+	if(argc != 3)
+	{
+		perror("Not a valid number of arguments");
+		exit(0);
+	}
+	struct Package* package = malloc( sizeof( struct Package));
+	buffer = malloc( BUFFER * sizeof(char));
+	package->key = atoi(argv[1]);
 
+	if( strcmp(argv[2],"-e") != 0  && strcmp(argv[2],"-d") != 0 )
+	{
+		printf("%s",argv[2]);
+		perror("Not a valid flag");
+		exit(0);
+	}
+	package->flag[0] = '-';
+	package->flag[1] = (strcmp(argv[1], "-e")) ? 'e' : 'd';
+	int size = read(0,buffer,BUFFER);	
+	puts("Making threadpool with 4 threads");
+	threadpool thpool = thpool_init(4);
+
+	puts("Adding 4 tasks to threadpool");
+	
+    for (int i=0; i < 40; i++){
+		thpool_add_work(thpool, WritingToFile, (void*)(Package*)package);
+	};
+	
+	
+
+	thpool_wait(thpool);
+	puts("Killing threadpool");
+	thpool_destroy(thpool);
+	free(package);
+	free(buffer);
 
 	return 0;
 }
